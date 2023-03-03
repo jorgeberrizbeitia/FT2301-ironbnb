7e0a55b9fed117056feaf1f2af4fa3660866350d
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-03-03 12:57:05 +0100

    form done

--- src/pages/AptAddForm.jsx
+++ src/pages/AptAddForm.jsx
@@ -1,8 +1,50 @@
-
+import { useState } from "react"
 
 function AptAddForm() {
+
+  const [ title, setTitle ] = useState("")
+  const [ img, setImg ] = useState("")
+  const [ pricePerDay, setPricePerDay ] = useState(0)
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleImgChange = (event) => {
+    setImg(event.target.value)
+  }
+
+  const handlePricePerDayChange = (event) => {
+    setPricePerDay(event.target.value)
+  }
+
   return (
-    <div>AptAddForm</div>
+    <div>
+      
+      <form>
+
+        <label htmlFor="title">Titulo</label>
+        <input type="text" name="title" onChange={handleTitleChange}/>
+
+        <br />
+
+        <label htmlFor="img">Imagen</label>
+        <input type="text" name="img" onChange={handleImgChange}/>
+
+        <br />
+
+        <label htmlFor="pricePerDay">Precio por día</label>
+        <input type="number" name="pricePerDay" onChange={handlePricePerDayChange}/>
+
+        <br />
+
+        <button>Agregar</button>
+
+
+      </form>
+
+
+    </div>
   )
 }
 
--- src/pages/AptList.jsx
+++ src/pages/AptList.jsx
@@ -1,16 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom" // manipular el URL del usuario
 
-
+// Este componente renderizará data de un backend
 function AptList() {
+
+  const navigate = useNavigate()
+
+  // 1. Creamos el estado que almacenará la data
+  const [ allApartments, setAllApartments ] = useState(null)
+  const [ isFetching, setIsFetching ] = useState(true) // este estado determina si el componente está buscando data o no
+
+  // 2. invocamos el useEffect para hacer la llamada justo cuando el componente se ha montado correctamente
+  useEffect(() => {
+
+    // 3. hacer la llamada al backend para verificar que recibimos la data
+    getData()
+
+  }, [])
+
+  // 3.2 tenemos que crear la funcion externa si queremos trabajar con async/await
+  const getData = async () => {
+    try {
+      
+      const response = await axios.get("https://ironbnb-m3.herokuapp.com/apartments")
+      console.log(response)
+      setAllApartments(response.data)
+      setIsFetching(false) // ya tengo la data
+
+    } catch (error) {
+      console.log(error)
+      // vamos a hacer una redirección del usuario a "/error"
+      navigate("/error")
+    }
+  }
+
+  // clausula de guardia
+  if (isFetching === true) {
+    return <h3>... buscando</h3>
+    // no hagas más despues de esta linea
+  } 
+
   return (
     <div>
       
       <h2>Aqui los pisos disponibles</h2>
 
-      <div>
-        <h3>Nombre de piso</h3>
-        <p>Descripción</p>
-        <img src="" alt="" />
-      </div>
+  
+
+      {allApartments.map((eachApt) => {
+        return (
+        <div key={eachApt._id}>
+          <h3>{eachApt.title}</h3>
+          <p>Precio: {eachApt.pricePerDay}</p>
+          <img src={eachApt.img} alt="img" width={200}/>
+        </div>
+        )
+      })}
+
 
     </div>
   )
